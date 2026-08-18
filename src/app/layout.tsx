0346c05a2c88863b1c026fe0d836a55454641f30
Urvil Patel <uvpatel7271@gmail.com>
2026-08-18 22:34:05 +0530

    Implemented dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <TooltipProvider>
+
+       
         <ClerkProvider>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <Show when="signed-out">
@@ -41,6 +45,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           </header>
           {children}
         </ClerkProvider>
+         </TooltipProvider>
       </body>
     </html>
   );
