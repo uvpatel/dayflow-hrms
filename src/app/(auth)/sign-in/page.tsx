cbe26330955fcc1995a4b3ab0efde2499efede49
Urvil Patel <uvpatel7271@gmail.com>
2026-08-22 09:10:00 +0530

    sign-in page added

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { LoginForm } from "@/components/login-form";
+
+export default async function SignInPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
+
